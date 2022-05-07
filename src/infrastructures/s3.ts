7d18b62a047289142b92c8de2ed5ac8a2ffa1047
OHNISHI MAYUMI <mayumi.ohnishi@onewedge.co.jp>
2022-05-07 19:15:20 +0900

    change: directory name (lambda/ → src/)

--- src/infrastructures/s3.ts
+++ src/infrastructures/s3.ts
@@ -1,25 +1,74 @@
-import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
-import type { S3ClientConfig, GetObjectRequest, PutObjectRequest } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  HeadBucketCommand,
+  HeadObjectCommand,
+  CreateBucketCommand
+} from '@aws-sdk/client-s3';
+import type { S3ClientConfig, GetObjectRequest, PutObjectCommandInput } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 type Credentials = {
   accessKeyId: string;
   secretAccessKey: string;
-  sessionToken: string;
+  sessionToken?: string;
 };
 
 export class S3Serializer {
   private readonly s3: S3Client;
   constructor(readonly region: string, assumeRoleCredential?: Credentials) {
-    const params: S3ClientConfig = {
+    let params: S3ClientConfig;
+
+    params = {
       apiVersion: '2006-03-01',
-      region: region
+      region: region,
+      // ! Add to test on LocalStack (#endpoint & #forcePathStyle)
+      endpoint: 'http://localhost:4566',
+      forcePathStyle: true
     };
     if (assumeRoleCredential) {
       params.credentials = assumeRoleCredential;
     }
-
     this.s3 = new S3Client(params);
+  }
+
+  /**
+   * 指定のS3バケット or バケット内にオブジェクト存在有無を返却する
+   * @returns 存在する: true / 存在しない: false
+   */
+  async exists(bucketName: string, objectName?: string) {
+    try {
+      if (objectName) {
+        await this.s3.send(
+          new HeadObjectCommand({
+            Bucket: bucketName,
+            Key: objectName
+          })
+        );
+        return true;
+      }
+
+      await this.s3.send(
+        new HeadBucketCommand({
+          Bucket: bucketName
+        })
+      );
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  /**
+   * バケット新規作成
+   */
+  createBucket(bucketName: string) {
+    return this.s3.send(
+      new CreateBucketCommand({
+        Bucket: bucketName
+      })
+    );
   }
 
   /**
@@ -31,7 +80,6 @@ export class S3Serializer {
         Bucket: bucketName,
         Key: objectName
       };
-
       const output = await this.s3.send(new GetObjectCommand(params));
       // Convert Body from a Buffer to a String.
       return output.Body?.toString();
@@ -67,18 +115,17 @@ export class S3Serializer {
   put(
     bucketName: string,
     objectName: string,
-    data: PutObjectRequest['Body'],
+    data: PutObjectCommandInput['Body'],
     contentType = 'application/json',
     storageClass = 'STANDARD'
   ) {
-    const params: PutObjectRequest = {
+    const params: PutObjectCommandInput = {
       Bucket: bucketName,
       Key: objectName,
       Body: data,
       ContentType: contentType,
       StorageClass: storageClass
     };
-
     return this.s3.send(new PutObjectCommand(params));
   }
 
